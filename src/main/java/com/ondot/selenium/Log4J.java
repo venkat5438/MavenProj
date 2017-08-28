79ce47317a8ae9584b3dbc836ff839fa5f581740
venkat5438 <venkatramreddy108@gmail.com>
2017-08-28 12:13:42 -0300

    added

--- src/main/java/com/ondot/selenium/Log4J.java
+++ src/main/java/com/ondot/selenium/Log4J.java
@@ -2,10 +2,10 @@ package com.ondot.selenium;
 
 import java.util.List;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.SimpleLayout;
@@ -23,26 +23,27 @@ public class Log4J {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
-		 System.setProperty("org.apache.logging.log4j.simplelog.StatusLogger.level", "org.apache.commons.logging.impl.Jdk14Logger");
-		 BasicConfigurator.configure();
-	     //Logger OurLogger = LogManager.getLogger("OurLogger");
-	     PropertyConfigurator.configure("log4j.properties");
+		 //System.setProperty("org.apache.logging.log4j.simplelog.StatusLogger.level", "org.apache.commons.logging.impl.Jdk14Logger");
+		 //BasicConfigurator.configure();
+	       Logger OurLogger = LogManager.getLogger("OurLogger");
+	       PropertyConfigurator.configure("log4j.properties");
 	     
 	         // below is file appender
 		    FileAppender fileappender=new FileAppender();
 		    fileappender.setFile("E://Log4j//logfile.txt");
 		    fileappender.setLayout(new SimpleLayout());
-		    Log.addAppender(fileappender);//OurLogger
+		    OurLogger.addAppender(fileappender);//OurLogger
 		    fileappender.activateOptions();
 		    
 		    //below following code sets the logger to the info level, which means all messages with severe, warning and info will be logged.
-		    Log.setLevel(Level.INFO);
+		   // Log.setLevel(Level.INFO);
+		    OurLogger.setLevel(Level.ALL);
 		    
 		    
 		    // below is console appender
 		    ConsoleAppender ConsoleAppender = new ConsoleAppender();
 		     ConsoleAppender.setLayout(new SimpleLayout());
-		     Log.addAppender(ConsoleAppender);
+		     OurLogger.addAppender(ConsoleAppender);
 	         ConsoleAppender.activateOptions();
 	         
 	         
@@ -66,26 +67,41 @@ public class Log4J {
 		     //DOMConfigurator.configure("log4j.xml");
 				System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\Lib\\chromedriver.exe");
 				driver = new ChromeDriver();
-				
 				Log.info("New driver instantiated");
-				driver.get("https://dev-timesheets.mynisum.com/");
-				Thread.sleep(5000);
+				driver.get("https://qa-timesheets.mynisum.com");
+				Thread.sleep(2000);
 				Log.info("sleep applied on the driver for 5 seconds");
 				List<WebElement> alllinks=driver.findElements(By.tagName("a"));
+				Thread.sleep(3000);
 				int count=alllinks.size();
-				System.out.println("no of links "+ count);
+				Log.debug("counted all links");
+				System.out.println("printed count"+ count);
+				Log.debug(alllinks);
+				Log.debug("count printed");
 				Log.info("above printed no of links count");
-		     /* for (int i = 0; i < count; i++) {
+		     
+				 for (int i = 0; i < count; i++) {
 		     System.out.println(alllinks.get(i).getAttribute("href"));
+		     Log.debug("href");
+		     Log.debug("href");
+		     Log.debug("href");
+		     Log.debug("href");
 		     AutoUtils.checkResponse(alllinks.get(i).getAttribute("href"));
-		     } */
+		     Log.debug("href");
+		     } 
 				
 				
-				for (WebElement webElement : alllinks) 
+				/*for (WebElement webElement : alllinks) 
 				{
 					System.out.println(webElement.getAttribute("href"));
+					Log.debug("href");
+					System.out.println(webElement.getTagName());
+					Log.debug("href");
+					System.out.println(webElement.getText());
+					Log.debug("href");
 					AutoUtils.checkResponse(webElement.getAttribute("href"));
-					}
+					Log.debug("href");
+					}*/
 				driver.quit();
 				Log.info("we closed the browser");
 		
